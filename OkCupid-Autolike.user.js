2cc6a89eb3b3a17f215451a0dc96686a1e7f5a4a
Will Wilkins <aeoneon@gmail.com>
2017-07-29 11:44:23 -0500

    Updated includes to match newer okcupid URLs and HTTPS
    They changed the name from quickmatch to doubletake. I also added https compatibility.

--- OkCupid-Autolike.user.js
+++ OkCupid-Autolike.user.js
@@ -2,6 +2,11 @@
 // @name OkCupid-Autolike-Autolike
 // @author nemanjan00
 // @include http://www.okcupid.com/quickmatch
+// @include https://www.okcupid.com/quickmatch
+// @include http://www.okcupid.com/doubletake
+// @include https://www.okcupid.com/doubletake
+// @include https://www2.okcupid.com/doubletake
+// @include https://www2.okcupid.com/quickmatch
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/OkCupid-Autolike/master/OkCupid-Autolike.user.js
 // @namespace https://github.com/nemanjan00/OkCupid-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/OkCupid-Autolike/master/OkCupid-Autolike.user.js
