047f13c5b0f9e2b35948e6785ce74803d3464694
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-08-19 18:58:50 +0200

    Mobile

--- OkCupid-Autolike.user.js
+++ OkCupid-Autolike.user.js
@@ -7,12 +7,16 @@
 // @include https://www.okcupid.com/doubletake
 // @include https://www2.okcupid.com/doubletake
 // @include https://www2.okcupid.com/quickmatch
+// @include https://m.okcupid.com/doubletake
 // @downloadURL https://raw.githubusercontent.com/nemanjan00/OkCupid-Autolike/master/OkCupid-Autolike.user.js
 // @namespace https://github.com/nemanjan00/OkCupid-Autolike
 // @updateURL https://raw.githubusercontent.com/nemanjan00/OkCupid-Autolike/master/OkCupid-Autolike.user.js
-// @version 2.1
+// @version 2.2
 // ==/UserScript==
 
 setInterval(function(){
-	document.getElementsByClassName("cardactions-action--like")[0].click();
-}, 50);
+	var desktopButton = document.getElementsByClassName("cardactions-action--like")[0];
+	var mobileButton = document.getElementsByClassName("quickmatch-button-like")[0];
+	
+	(desktopButton || mobileButton).click();
+}, 500);
